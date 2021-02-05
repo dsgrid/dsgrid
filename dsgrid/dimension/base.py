3bfb51529c07196d2503d888ff3c27fd7d1408de
mmooney <meghan.mooney@nrel.gov>
2021-02-04 21:51:00 -0700

    add new dimensions and update type names

--- dsgrid/dimension/base.py
+++ dsgrid/dimension/base.py
@@ -6,30 +6,63 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class DSGBaseDimension(abc.ABC):
+class DSGBaseDimension(abc.ABC):    # TODO: Add Type here?
     """Base class for all dsgrid dimenions"""
     id: str
     name: str
 
 
 @dataclass(frozen=True)
-class EndUseDimension(DSGBaseDimension, abc.ABC):
+class EndUseDimensionType(DSGBaseDimension, abc.ABC):
     """Base class for all end use dimenions"""
 
 
 @dataclass(frozen=True)
-class GeographicDimension(DSGBaseDimension, abc.ABC):
+class GeographicDimensionType(DSGBaseDimension, abc.ABC):
     """Base class for all geography dimensions"""
 
 
 @dataclass(frozen=True)
-class SectorDimension(DSGBaseDimension, abc.ABC):
+class ModelDimensionType(DSGBaseDimension, abc.ABC):
+    """Base class for all load model dimenions"""
+
+
+@dataclass(frozen=True)
+class ModelYearDimensionType(DSGBaseDimension, abc.ABC):
+    """Base class for all model year dimensions"""
+
+
+@dataclass(frozen=True)
+class ScenarioDimensionType(DSGBaseDimension, abc.ABC):
+    """Base class for all scenario dimensions"""
+
+
+@dataclass(frozen=True)
+class SectorDimensionType(DSGBaseDimension, abc.ABC):
     """Base class for all sector dimenions"""
 
 
 @dataclass(frozen=True)
-class TimeDimension(DSGBaseDimension, abc.ABC):
+class SubSectorDimensionType(DSGBaseDimension, abc.ABC):
+    """Base class for all subsector dimenions"""
+
+
+@dataclass(frozen=True)
+class TimeDimensionType(DSGBaseDimension, abc.ABC):
     """Base class for all time dimenions"""
+
+
+@dataclass(frozen=True)
+class WeatherDimensionType(DSGBaseDimension, abc.ABC):
+    """ attributes"""
+
+
+
+
+
+
+
+
 
 
 class DayType(enum.Enum):

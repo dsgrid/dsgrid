84ef3a0a03d33f981196ec7f4869fce1b113cfa5
Daniel Thom <daniel.thom@nrel.gov>
2021-05-06 16:44:44 -0600

    Add collecting of timer stats

--- dsgrid/__init__.py
+++ dsgrid/__init__.py
@@ -1,1 +1,2 @@
 from ._version import __version__, __author__, __copyright__
+from dsgrid.utils.timing import timer_stats_collector
--- dsgrid/cli/dsgrid.py
+++ dsgrid/cli/dsgrid.py
@@ -4,6 +4,7 @@ import logging
 
 import click
 
+from dsgrid.utils.timing import timer_stats_collector
 from dsgrid.cli.download import download
 from dsgrid.cli.query import query
 from dsgrid.cli.registry import registry
@@ -23,6 +24,20 @@ def cli(log_file, verbose):
     """dsgrid commands"""
     level = logging.DEBUG if verbose else logging.INFO
     setup_logging("dsgrid", log_file, console_level=level, file_level=level)
+
+
+@cli.resultcallback()
+def callback(*args, **kwargs):
+    # Raise the console level so that timer stats only go to the log file.
+    dsgrid_logger = logging.getLogger("dsgrid")
+    for i, handler in enumerate(dsgrid_logger.handlers):
+        if handler.name == "console":
+            handler.setLevel(logging.WARNING)
+            break
+
+    timer_stats_collector.log_stats()
+
+    # Leave the console logger changed because the process will now exit.
 
 
 cli.add_command(download)

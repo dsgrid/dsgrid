6851b0d7fd37dfb666d562f42121441ba4533319
Daniel Thom <daniel.thom@nrel.gov>
2021-08-17 09:14:42 -0600

    Change tests to use submodule

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 
 from dsgrid.utils.run_command import check_run_command
 from dsgrid.utils.spark import init_spark
@@ -11,39 +10,19 @@ from dsgrid.tests.common import (
 )
 
 
-TEST_DATA_BRANCH = "main"
-
-
 def pytest_sessionstart(session):
-    if TEST_PROJECT_PATH.exists():
-        set_current_branch(TEST_PROJECT_PATH, TEST_DATA_BRANCH)
+    if not os.listdir(TEST_PROJECT_PATH):
+        print("The {dsgrid-test-data} submodule has not been initialized. "
+              "Please run these commands:")
+        print("git submodule init")
+        print("git submodule update")
+
+    if TEST_REGISTRY.exists():
+        print(f"Use existing test registry at {TEST_REGISTRY}.")
     else:
         check_run_command(
-            f"git clone https://github.com/dsgrid/dsgrid-test-data.git {TEST_PROJECT_PATH}"
-        )
-        set_current_branch(TEST_PROJECT_PATH, TEST_DATA_BRANCH)
-
-    if not TEST_REGISTRY.exists():
-        check_run_command(
-            f"python tests/make_us_data_registry.py {TEST_REGISTRY} -p {TEST_PROJECT_REPO} -d {TEST_DATASET_DIRECTORY}"
+            f"python tests/make_us_data_registry.py {TEST_REGISTRY} -p {TEST_PROJECT_REPO} "
+            f"-d {TEST_DATASET_DIRECTORY}"
         )
 
     init_spark("dsgrid-test")
-
-
-def set_current_branch(repo_path, branch):
-    orig = os.getcwd()
-    os.chdir(repo_path)
-    try:
-        cur_branch = get_current_branch()
-        if cur_branch != branch:
-            check_run_command(f"git fetch origin {branch}")
-            check_run_command(f"git checkout {branch}")
-    finally:
-        os.chdir(orig)
-
-
-def get_current_branch():
-    output = {}
-    check_run_command("git rev-parse --abbrev-ref HEAD", output=output)
-    return output["stdout"].strip()

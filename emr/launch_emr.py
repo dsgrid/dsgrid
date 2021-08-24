181b78cabe92ee29c619548538f9dffd096a1e2d
roliveir <ricardo.oliveira@nrel.gov>
2021-08-24 16:40:24 -0600

    cleanup launch script

--- emr/launch_emr.py
+++ emr/launch_emr.py
@@ -11,8 +11,6 @@ import yaml
 import git
 import paramiko
 
-# import pushbullet
-
 
 def launchemr(name=None):
 
@@ -24,24 +22,20 @@ def launchemr(name=None):
     with open(os.path.join(here, "emr_config.yml"), "r") as f:
         cfg = yaml.safe_load(f)
 
-    # TODO: what is this? this might be where outputs go
     cluster_id_filename = os.path.join(here, "running_cluster_id.txt")
 
     # this is moving the bootstrap-dask file to S3
     fs = s3fs.S3FileSystem()
     s3_scratch = cfg["s3_scratch"].strip().rstrip("/")
     bootstrap_script_loc = f"{s3_scratch}/bootstrap-pyspark"
-    bootstrap_dsgrid_loc = f"{s3_scratch}/bootstrap-dsgrid"
-    local_bootstrap_dask = os.path.join(here, "bootstrap-pyspark")
-    local_bootstrap_dsgrid = os.path.join(here, "bootstrap-dsgrid")
-    fs.put(local_bootstrap_dask, bootstrap_script_loc)
-    fs.put(local_bootstrap_dsgrid, bootstrap_dsgrid_loc)
+    local_bootstrap_pyspark = os.path.join(here, "bootstrap-pyspark")
+    fs.put(local_bootstrap_pyspark, bootstrap_script_loc)
 
     emr = boto3.client("emr")
 
     if os.path.exists(cluster_id_filename):
         with open(cluster_id_filename, "rt") as f:
-            job_flow_id = f.read()  # TODO: what is this?
+            job_flow_id = f.read()
     else:
         resp = emr.run_job_flow(
             Name=f"{getpass.getuser()}-dsgrid",
@@ -50,7 +44,6 @@ def launchemr(name=None):
             Instances={
                 "InstanceGroups": [
                     {
-                        # Do we want on-demand or spot for master? maybe provide an option for both
                         "Market": "ON_DEMAND",
                         "InstanceRole": "MASTER",
                         "InstanceType": cfg.get("master_instance", {}).get(
@@ -69,9 +62,7 @@ def launchemr(name=None):
                         ),
                     },
                 ],
-                # cfg['ssh_keys']['key_name'], # TODO: check this
                 "Ec2KeyName": cfg["ssh_keys"]["key_name"],
-                # If parameter is set to TRUE , the cluster transitions to the WAITING state rather than shutting down after the steps have completed.
                 "KeepJobFlowAliveWhenNoSteps": True,
                 "Ec2SubnetId": cfg.get("subnet_id"),
             },
@@ -84,24 +75,17 @@ def launchemr(name=None):
                         "Args": [],
                     },
                 },
-                # {
-                #     "Name": "installDsGrid",
-                #     "ScriptBootstrapAction": {
-                #         "Path": bootstrap_dsgrid_loc,
-                #         "Args": [],
-                #     },
-                # },
             ],
-            VisibleToAllUsers=True,  # TODO: what is this?
-            EbsRootVolumeSize=80,  # TODO: what is this?
-            JobFlowRole="EMR_EC2_DefaultRole",  # TODO: what is this?
-            ServiceRole="EMR_DefaultRole",  # TODO: what is this?
+            VisibleToAllUsers=True,
+            EbsRootVolumeSize=80,
+            JobFlowRole="EMR_EC2_DefaultRole",
+            ServiceRole="EMR_DefaultRole",
             Tags=[
                 {"Key": "billingId", "Value": str(cfg.get("billing_id"))},
                 {"Key": "project", "Value": "dsgrid"},
             ],
         )
-        job_flow_id = resp["JobFlowId"]  # TODO: what is this?
+        job_flow_id = resp["JobFlowId"]
         with open(cluster_id_filename, "wt") as f:
             f.write(job_flow_id)
         time.sleep(5)
@@ -118,7 +102,6 @@ def launchemr(name=None):
             break
         time.sleep(30)
 
-    # pushbullet.push_message(f'EMR Cluster is {state}', message)
     master_instance = emr.list_instances(
         ClusterId=job_flow_id, InstanceGroupTypes=["MASTER"]
     )
@@ -129,8 +112,8 @@ def launchemr(name=None):
         os.path.expanduser(cfg["ssh_keys"]["pkey_location"])
     )
 
-    cnopts = pysftp.CnOpts()  # TODO: What is this?
-    cnopts.hostkeys = None  # TODO: What is this?
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
 
     print("Copying AWS config files")
     aws_credentials = os.path.join(here, "credentials")
@@ -149,7 +132,7 @@ def launchemr(name=None):
     dsgrid_repo = git.Repo.clone_from(
         "https://github.com/dsgrid/dsgrid.git",
         ds_grid_repo_path,
-        branch="develop",
+        branch=str(cfg.get("repo_branch")),
     )
 
     print("Copying notebooks and code to master node")
@@ -199,20 +182,14 @@ def launchemr(name=None):
         ip_address, username="hadoop", private_key=mypkey, cnopts=cnopts
     ) as sftp:
         sftp.get_r("code", here)
-    # extra_clustermaker = os.path.join(
-    #     here, "notebooks", "clustermaker.py"
-    # )  # TODO: what is this?
-    # if os.path.exists(extra_clustermaker):
-    #     os.remove(extra_clustermaker)
 
     resp = input("Terminate cluster [y/n]? ")
     if resp.lower().startswith("y"):
         print(f"Terminating cluster {job_flow_id}")
         emr.terminate_job_flows(JobFlowIds=[job_flow_id])
         os.remove(cluster_id_filename)
-        if not resp.lower().endswith("k"):  # deletes s3 scratch
+        if not resp.lower().endswith("k"):
             fs.rm(s3_scratch, recursive=True)
-
 
 if __name__ == "__main__":
     launchemr()

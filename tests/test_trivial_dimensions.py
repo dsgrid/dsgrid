38292b79375cfa415b4c57a0d87d019b9c793004
mooneyme <meghan.mooney@nrel.gov>
2021-08-31 13:04:27 -0600

    test for trivial dimensions that are bad

---
+++ tests/test_trivial_dimensions.py
@@ -0,0 +1,37 @@
+import os
+from pyspark.sql.functions import log
+import pytest
+
+from dsgrid.config.dataset_config import DatasetConfig
+from dsgrid.exceptions import DSGInvalidDimension
+from dsgrid.tests.common import create_local_test_registry, make_test_project_dir
+from dsgrid.utils.files import load_data, dump_data
+from dsgrid.tests.common import make_test_project_dir, make_test_data_dir, TEST_DATASET_DIRECTORY
+from tests.make_us_data_registry import make_test_data_registry, replace_dataset_path
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+
+def test_trivial_dimension_bad(make_test_project_dir, make_test_data_dir):
+    """Test bad trivial dimensions where county geography is set to trivial"""
+    with TemporaryDirectory() as tmpdir:
+        base_dir = Path(tmpdir)
+        manager = make_test_data_registry(
+            base_dir,
+            make_test_project_dir,
+            dataset_path=make_test_data_dir,
+            include_datasets=False,
+        )
+        dataset_dir = make_test_project_dir / "datasets" / "sector_models" / "comstock"
+        assert dataset_dir.exists()
+        dataset_config_file = dataset_dir / "dataset.toml"
+
+        config = load_data(dataset_config_file)
+        config["trivial_dimensions"] = config["trivial_dimensions"] + ["geography"]
+        dump_data(config, dataset_config_file)
+
+        with pytest.raises(DSGInvalidDimension):
+            manager.dataset_manager.register(
+                config_file=dataset_config_file, submitter="test", log_message="test"
+            )

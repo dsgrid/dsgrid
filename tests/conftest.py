0eae675f420c5946cda9f2ae7019bf816c3e1a5e
Daniel Thom <daniel.thom@nrel.gov>
2021-09-07 17:26:28 -0600

    Record last commit used to create test registry
    This fixes a problem that has caused a nuisance when the test registry
    becomes stale and causes tests to fail.
    
    The code will now record the commit used to create the registry. It will
    re-create the registry on subsequent tests if newer commits occur.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import os
+import re
 import shutil
 import sys
 
-from dsgrid.utils.run_command import run_command
+from dsgrid.utils.run_command import run_command, check_run_command
 from dsgrid.utils.spark import init_spark
 from dsgrid.tests.common import (
     TEST_PROJECT_PATH,
@@ -14,14 +15,24 @@ from dsgrid.tests.common import (
 
 def pytest_sessionstart(session):
     if not os.listdir(TEST_PROJECT_PATH):
-        print("The {dsgrid-test-data} submodule has not been initialized. "
-              "Please run these commands:")
+        print(
+            "The {dsgrid-test-data} submodule has not been initialized. "
+            "Please run these commands:"
+        )
         print("git submodule init")
         print("git submodule update")
 
-    if TEST_REGISTRY.exists():
+    commit_file = TEST_REGISTRY / "commit.txt"
+    latest_commit = _get_latest_commit()
+    if (
+        TEST_REGISTRY.exists()
+        and commit_file.exists()
+        and commit_file.read_text().strip() == latest_commit
+    ):
         print(f"Use existing test registry at {TEST_REGISTRY}.")
     else:
+        if TEST_REGISTRY.exists():
+            shutil.rmtree(TEST_REGISTRY)
         ret = run_command(
             f"python tests/make_us_data_registry.py {TEST_REGISTRY} -p {TEST_PROJECT_REPO} "
             f"-d {TEST_DATASET_DIRECTORY}"
@@ -30,5 +41,15 @@ def pytest_sessionstart(session):
             # Delete it because it is invalid.
             shutil.rmtree(TEST_REGISTRY)
             sys.exit(1)
+        commit_file.write_text(latest_commit + "\n")
 
     init_spark("dsgrid-test")
+
+
+def _get_latest_commit():
+    output = {}
+    check_run_command("git log -n 1", output=output)
+    match = re.search(r"^commit (\w+)", output["stdout"])
+    assert match, output
+    commit = match.group(1)
+    return commit

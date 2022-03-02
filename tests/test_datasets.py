9ba402768cdeebc53e8bf8a7681281e4e7d53658
Daniel Thom <daniel.thom@nrel.gov>
2022-03-02 14:44:33 -0700

    Remove test-generated file

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -96,6 +96,9 @@ def test_invalid_datasets(make_test_project_dir, make_test_data_dir):
             finally:
                 if test_dir.exists():
                     shutil.rmtree(test_dir)
+                missing_record_file = Path(f"{DATASET_ID}__missing_dimension_record_combinations.csv")
+                if missing_record_file.exists():
+                    shutil.rmtree(missing_record_file)
 
 
 def _setup_invalid_load_data_lookup_column_name(data_dir):

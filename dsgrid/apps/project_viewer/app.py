6d9882d791256faf071374d3de3fc9b60f8558db
Daniel Thom <daniel.thom@nrel.gov>
2022-10-06 08:39:17 -0600

    Use Bootstrap for styling

--- dsgrid/apps/project_viewer/app.py
+++ dsgrid/apps/project_viewer/app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 from dash import Dash, dash_table, dcc, html, Input, Output, State
+import dash_bootstrap_components as dbc
+
 from dash.exceptions import PreventUpdate
 
 from dsgrid.api.response_models import ListProjectsResponse
@@ -8,32 +10,68 @@ from dsgrid.api.response_models import ListProjectsResponse
 
 DSGRID_API_URL = "http://127.0.0.1:8000"
 
-app = Dash(__name__)
+# Copied DataTable styles from https://gist.github.com/marcogoldin/8fc4c3945cef17ca38d55c4e17ebbbe6
+STYLE_TABLE = {
+    "fontFamily": '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,'
+    '"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color '
+    'Emoji"'
+}
+STYLE_HEADER = {"backgroundColor": "white", "fontWeight": "bold", "padding": "0.75rem"}
+STYLE_CELL = {
+    "fontFamily": STYLE_TABLE["fontFamily"],
+    "fontWeight": "400",
+    "lineHeight": "1.5",
+    "color": "#212529",
+    "textAlign": "left",
+    "whiteSpace": "normal",
+    "height": "auto",
+    "padding": "0.75rem",
+    "border": "1px solid #dee2e6",
+    "verticalAlign": "top",
+}
+STYLE_DATA_CONDITIONAL = [{"if": {"row_index": "odd"}, "backgroundColor": "#f8f9fa"}]
 
-app.layout = html.Div(
-    children=[
-        html.H1(children="dsgrid Project Viewer", className="app-header"),
+app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
+app.layout = dbc.Container(
+    [
+        dbc.Alert("dsgrid Project Viewer", color="info"),
         html.Div(
             [
-                "dsgrid URL: ",
+                "URL: ",
                 dcc.Input(
                     id="url_text",
                     value=DSGRID_API_URL,
                 ),
-                "Status: ",
+                html.Label("Status:", style={"margin-left": "15px", "margin-right": "15px"}),
                 dcc.Input(
                     id="status_text",
                     value="disconnected",
                 ),
+                dbc.Button(
+                    "Connect",
+                    outline=True,
+                    color="primary",
+                    id="connect_button",
+                    n_clicks=0,
+                    style={"margin-left": "15px"},
+                ),
+            ]
+        ),
+        html.Br(),
+        html.Div(
+            [
+                "Select a project",
+                dcc.Dropdown([], "", id="project_dd"),
             ],
         ),
-        html.Button("Connect", id="connect_button", n_clicks=0),
         html.Br(),
-        html.H5("Select a project"),
-        dcc.Dropdown([], "", id="project_dd"),
-        html.Br(),
-        html.Button("List dimensions", id="list_dimensions_button", n_clicks=0),
-        html.Br(),
+        dbc.Button(
+            "List dimensions",
+            outline=True,
+            color="primary",
+            id="list_dimensions_button",
+            n_clicks=0,
+        ),
         html.Div(
             [
                 dash_table.DataTable([], [], id="dimensions_table"),
@@ -41,23 +79,26 @@ app.layout = html.Div(
             ],
         ),
         html.Br(),
+        html.H5("Dimension Records"),
+        dcc.Input(
+            id="dimension_display_name",
+            value="",
+            readOnly=True,
+        ),
         html.Div(
             [
-                html.H5("Dimension Records", className="app-header"),
-                dcc.Input(
-                    id="dimension_display_name",
-                    value="",
-                    readOnly=True,
-                ),
-                html.Div(
-                    [
-                        dash_table.DataTable([], [], id="dimension_records_table"),
-                        html.Div(id="dimension_records_table_container"),
-                    ],
-                ),
-            ],
+                dash_table.DataTable([], [], id="dimension_records_table"),
+                html.Div(id="dimension_records_table_container"),
+            ]
         ),
     ],
+    className="m-5",
+    # fluid=True,
+    # Per the docs, this should be set to True. However, that causes a display issue where the
+    # right part of the GUI is hidden and so you have to scroll horizontally to see it.
+    # When passing the allowed parameters, like lg, xl, xxl, the width is too small.
+    # Passing a non-supported string makes it work. This will almost certainly break in the future.
+    fluid="invalidparameter",
 )
 
 
@@ -102,26 +143,10 @@ def on_list_dimensions(n_clicks, project_id, url):
         sort_action="native",
         row_selectable="single",
         selected_rows=[],
-        style_cell={"textAlign": "left", "padding": "5px"},
-        style_header={
-            "backgroundColor": "white",
-            "fontWeight": "bold",
-            "border": "1px solid grey",
-        },
-        style_data_conditional=[
-            {
-                "if": {"row_index": "odd"},
-                "backgroundColor": "rgb(220, 220, 220)",
-            }
-        ],
-        style_data={
-            "border": "1px solid grey",
-            "width": "100px",
-            "minWidth": "100px",
-            "maxWidth": "100px",
-            "overflow": "hidden",
-            "textOverflow": "ellipsis",
-        },
+        style_table=STYLE_TABLE,
+        style_header=STYLE_HEADER,
+        style_cell=STYLE_CELL,
+        style_data_conditional=STYLE_DATA_CONDITIONAL,
     )
 
 
@@ -156,26 +181,10 @@ def on_list_dimension_records(row_indexes, row_data, url):
             editable=False,
             filter_action="native",
             sort_action="native",
-            style_cell={"textAlign": "left", "padding": "5px"},
-            style_header={
-                "backgroundColor": "white",
-                "fontWeight": "bold",
-                "border": "1px solid grey",
-            },
-            style_data_conditional=[
-                {
-                    "if": {"row_index": "odd"},
-                    "backgroundColor": "rgb(220, 220, 220)",
-                }
-            ],
-            style_data={
-                "border": "1px solid grey",
-                "width": "100px",
-                "minWidth": "100px",
-                "maxWidth": "100px",
-                "overflow": "hidden",
-                "textOverflow": "ellipsis",
-            },
+            style_table=STYLE_TABLE,
+            style_header=STYLE_HEADER,
+            style_cell=STYLE_CELL,
+            style_data_conditional=STYLE_DATA_CONDITIONAL,
         ),
         row_data[row_index]["display_name"],
     )

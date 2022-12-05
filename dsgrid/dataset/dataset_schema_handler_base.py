a8c2dd6f64093484f795140d2e267d9779b7479f
Daniel Thom <daniel.thom@nrel.gov>
2022-12-05 14:34:35 -0700

    Check for case where datasets have invalid columns
    The code was not checking for the case where a dataset has pivoted
    columns that are not defined in the project's dimension and not present
    in the dataset dimension mapping record file.

--- dsgrid/dataset/dataset_schema_handler_base.py
+++ dsgrid/dataset/dataset_schema_handler_base.py
@@ -130,17 +130,16 @@ class DatasetSchemaHandlerBase(abc.ABC):
                 mapping_config = self._dimension_mapping_mgr.get_by_id(
                     ref.mapping_id, version=ref.version
                 )
-                from_ids = set()
-                to_ids = set()
-                for record in mapping_config.model.records:
-                    if record.to_id is not None:
-                        from_ids.add(record.from_id)
-                        to_ids.add(record.to_id)
-
-                diff = from_ids.difference(columns)
+                records = mapping_config.get_records_dataframe()
+                from_ids = get_unique_values(records, "from_id")
+                to_ids = get_unique_values(
+                    records.select("to_id").filter("to_id IS NOT NULL"), "to_id"
+                )
+                diff = from_ids.symmetric_difference(columns)
                 if diff:
                     raise DSGInvalidDataset(
-                        f"Dimension_mapping={mapping_config.config_id} has more from_id records than the dataset pivoted {dim_type.value} dimension: {diff}"
+                        f"Dimension_mapping={mapping_config.config_id} does not have the same "
+                        f"record IDs as the dataset={self._config.config_id} columns: {diff}"
                     )
                 return to_ids
 

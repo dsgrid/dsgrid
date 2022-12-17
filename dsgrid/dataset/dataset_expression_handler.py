f5af740eb1a1b395dc2e7acf8917e767d7aab611
Daniel Thom <daniel.thom@nrel.gov>
2022-12-17 13:26:49 -0700

    Use local version of py_expression_eval

--- dsgrid/dataset/dataset_expression_handler.py
+++ dsgrid/dataset/dataset_expression_handler.py
@@ -1,9 +1,9 @@
 import operator
 
-from py_expression_eval import Parser
 from pyspark.sql import DataFrame
 
 from dsgrid.exceptions import DSGInvalidOperation
+from dsgrid.utils.py_expression_eval import Parser
 
 
 class DatasetExpressionHandler:

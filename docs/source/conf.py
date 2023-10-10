593c5a0b8493acd29bf38b2d81d71953bbc5d19c
Elaine Hale <elaine.hale@nrel.gov>
2023-10-10 17:39:53 -0600

    Removing unnecessary imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,6 @@ release = dsgrid.__version__
 
 # -- General configuration ---------------------------------------------------
 
-from pygments.lexers.shell import BashSessionLexer, PowerShellSessionLexer
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.

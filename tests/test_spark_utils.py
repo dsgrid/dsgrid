2569ecc0bfece391192836113de53d68b04c553f
Daniel Thom <daniel.thom@nrel.gov>
2024-01-30 13:18:28 -0700

    Add test of restart_spark

--- tests/test_spark_utils.py
+++ tests/test_spark_utils.py
@@ -1,6 +1,10 @@
 from pyspark.sql import DataFrame
 
-from dsgrid.utils.spark import get_spark_session, try_read_dataframe
+from dsgrid.utils.spark import (
+    get_spark_session,
+    try_read_dataframe,
+    restart_spark_with_custom_conf,
+)
 
 
 def test_try_read_dataframe_invalid(tmp_path):
@@ -18,3 +22,24 @@ def test_try_read_dataframe_valid(tmp_path):
     df = try_read_dataframe(filename)
     assert isinstance(df, DataFrame)
     assert df.collect()[0].a == 1
+
+
+def test_restart_spark():
+    spark = get_spark_session()
+    cur_partitions = spark.conf.get("spark.sql.shuffle.partitions")
+    new_partitions = str(int(cur_partitions) + 1)
+    cur_compress = spark.conf.get("spark.rdd.compress")
+    if cur_compress.lower() == "true":
+        new_compress = "false"
+    elif cur_compress.lower() == "false":
+        new_compress = "true"
+    else:
+        assert False, cur_compress
+
+    conf = {
+        "spark.sql.shuffle.partitions": new_partitions,
+        "spark.rdd.compress": new_compress,
+    }
+    with restart_spark_with_custom_conf(conf=conf) as new_spark:
+        assert new_spark.conf.get("spark.sql.shuffle.partitions") == new_partitions
+        assert new_spark.conf.get("spark.rdd.compress") == new_compress

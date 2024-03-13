392e0258a814f61c433aebe6f740351fed428c16
Daniel Thom <daniel.thom@nrel.gov>
2024-03-13 09:45:14 -0600

    Fix test for representative time

--- tests/test_representative_time.py
+++ tests/test_representative_time.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
@@ -113,10 +114,16 @@ def test_time_consistency(one_weekday_day_and_one_weekend_day_per_month_by_hour_
     config.check_dataset_time_consistency(df, time_columns)
 
 
-def test_time_mapping(one_weekday_day_and_one_weekend_day_per_month_by_hour_table):
+@pytest.mark.parametrize("spark_time_zone", ["America/Los_Angeles"], indirect=True)
+def test_time_mapping(
+    one_weekday_day_and_one_weekend_day_per_month_by_hour_table, spark_time_zone
+):
+    # This test sets the Spark session time zone so that it can check times consistently
+    # across computers.
+    # It uses Pacific Prevailing Time to make the checks consistent with the dataset.
     df = one_weekday_day_and_one_weekend_day_per_month_by_hour_table
     # This dataset has only California counties.
-    df = df.withColumn("time_zone", F.lit("PacificStandard"))
+    df = df.withColumn("time_zone", F.lit("PacificPrevailing"))
     config = make_one_weekday_day_and_one_weekend_day_per_month_by_hour_config()
     project_time_config = make_date_time_config()
     mapped_df = config.convert_dataframe(df, project_time_config)
@@ -128,3 +135,48 @@ def test_time_mapping(one_weekday_day_and_one_weekend_day_per_month_by_hour_tabl
     expected_timestamps = [start + i * resolution for i in range(8760)]
     assert est_timestamps == expected_timestamps
     assert mapped_df.filter(f"{VALUE_COLUMN} IS NULL").rdd.isEmpty()
+
+    max_values = df.select("value").agg(F.max("value").alias("max")).collect()
+    assert len(max_values) == 1
+    max_value = max_values[0].max
+    max_row = df.filter(f"value == {max_value}").collect()[0]
+    # Expected max is determined by manually inspecting the file.
+    expected_max = 0.9995036580360138
+    assert math.isclose(max_value, expected_max)
+    expected_scenario = "scenario1"
+    expected_month = 3
+    expected_hour = 3
+    # -1 hour for period ending to beginning
+    expected_hour_pst = expected_hour - 1
+    expected_geo = "06073"
+    expected_model_year = "2030"
+    num_weekdays_in_march_2018 = 22
+    assert max_row.month == expected_month
+    assert max_row.hour == expected_hour
+    assert max_row.is_weekday
+    assert max_row.geography == expected_geo
+    assert max_row.model_year == expected_model_year
+    assert max_row.scenario == expected_scenario
+    mapped_df_at_max = mapped_df.filter(f"value == {max_value}").cache()
+    filtered_mapped_df = mapped_df.filter(
+        (F.col("model_year") == max_row.model_year)
+        & (F.col("geography") == max_row.geography)
+        & (F.col("scenario") == max_row.scenario)
+        & (F.month("timestamp") == max_row.month)
+        & (F.hour("timestamp") == expected_hour_pst)
+        & (F.weekday("timestamp") < 5)
+    ).cache()
+    try:
+        assert mapped_df_at_max.count() == num_weekdays_in_march_2018
+        assert filtered_mapped_df.count() == num_weekdays_in_march_2018
+        assert filtered_mapped_df.select("value").distinct().count() == 1
+        assert math.isclose(
+            filtered_mapped_df.select("value").distinct().collect()[0]["value"], expected_max
+        )
+        assert (
+            mapped_df_at_max.sort("timestamp").collect()
+            == filtered_mapped_df.sort("timestamp").collect()
+        )
+    finally:
+        mapped_df_at_max.unpersist()
+        filtered_mapped_df.unpersist()

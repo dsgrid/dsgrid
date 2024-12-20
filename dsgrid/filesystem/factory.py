f9923511f56c40c4626f7254ac0d186a6ca0a8d1
Daniel Thom <daniel.thom@nrel.gov>
2024-12-20 16:02:10 -0700

    Remove AWS packages to reduce dependencies
    We do not currently support AWS. This change can be reverted whenever we
    need to restore support.

--- dsgrid/filesystem/factory.py
+++ dsgrid/filesystem/factory.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 from .local_filesystem import LocalFilesystem
-from .s3_filesystem import S3Filesystem
 
-from dsgrid.common import AWS_PROFILE_NAME
+# from .s3_filesystem import S3Filesystem
+
+# from dsgrid.common import AWS_PROFILE_NAME
 
 
 def make_filesystem_interface(path):
@@ -21,9 +22,10 @@ def make_filesystem_interface(path):
     if isinstance(path, Path):
         path = str(path)
     if path.lower().startswith("s3"):
-        if path.startswith("S3"):
-            path = "s3" + path[2:]
-        fs_intf = S3Filesystem(path, AWS_PROFILE_NAME)
+        msg = f"Support for S3 is currently disabled: {path=}"
+        raise NotImplementedError(msg)
+        #    path = "s3" + path[2:]
+        # fs_intf = S3Filesystem(path, AWS_PROFILE_NAME)
     else:
         fs_intf = LocalFilesystem()
 

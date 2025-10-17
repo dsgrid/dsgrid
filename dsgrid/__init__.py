08be2c030e3f188e7b18419141604209447682f9
Elaine Hale <elaine.hale@nrel.gov>
2025-10-17 09:40:36 -0600

    Updating pyproject and __init__ for new release.

--- dsgrid/__init__.py
+++ dsgrid/__init__.py
@@ -1,11 +1,21 @@
+import datetime as dt
 import warnings
 
 from dsgrid.dsgrid_rc import DsgridRuntimeConfig
 from dsgrid.utils.timing import timer_stats_collector  # noqa: F401
 
-__version__ = "0.1.0"
-__copyright__ = "Copyright 2023, The Alliance for Sustainable Energy, LLC"
-__author__ = "Elaine Hale"
+__title__ = "dsgrid"
+__description__ = (
+    "Python API for registring and accessing demand-side grid model (dsgrid) datasets"
+)
+__url__ = "https://github.com/dsgrid/dsgrid"
+__version__ = "0.2.0"
+__author__ = "NREL"
+__maintainer_email__ = "elaine.hale@nrel.gov"
+__license__ = "BSD-3"
+__copyright__ = "Copyright {}, The Alliance for Sustainable Energy, LLC".format(
+    dt.date.today().year
+)
 
 warnings.filterwarnings("ignore", module="duckdb_engine")
 
